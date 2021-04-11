72a65c0dc51980eb04e8afe0ac289467f5409944
bremmurd <bremmurd08@gmail.com>
2021-04-11 14:38:50 +0300

    App Version 1.01

---
+++ src/Nav.js
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from "react";
+import "./Nav.css";
+
+function Nav() {
+  const [show, handleShow] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        handleShow(true);
+      } else handleShow(false);
+    });
+    return () => {
+      window.removeEventListener("scroll");
+    };
+  }, []);
+
+  return (
+    <div className={`nav ${show && "nav__black"}`}>
+      <img
+        className="nav__logo"
+        src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
+        alt="Logo"
+      />
+      <img
+        className="nav__avatar"
+        src="https://upload.wikimedia.org/wikipedia/commons/8/8c/UserAvatarChochem.png"
+        alt="User Avatar"
+      />
+    </div>
+  );
+}
+
+export default Nav;
